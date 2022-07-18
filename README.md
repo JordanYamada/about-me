9ccea72dce54f85ed2296b0b3f1ec280b9ea7573
Jordan Yamada <sadaotomokane@gmail.com>
2022-07-17 21:00:13 -0700

    added all content to HTML and the basic CSS

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # about-me
+
 Hanbyeol Lee Driver
 Jordan Yamada Navigator
+
+credit to the "No Onion" image at "http://imacomputa.org/onions/hater".
+A fellow onion hater, indeed.
--- js/app.js
+++ js/app.js
@@ -4,12 +4,14 @@ console.log('hello');
 
 let userName = prompt("Hello! What is your name?");
 alert("Welcome, "+userName+"! My name is Jordan Yamada.");
-alert("I was born in Portland, Oregon, but I grew up in Saipan, a small island in the Pacific");
+alert("I was born in Portland, Oregon, but I grew up in Saipan, a small island in the Pacific.");
 alert("I live in the Seattle area now, and I much prefer the weather out here. I do enjoy the grey, overcast skies and the gloomy, rainy days.");
 alert("I'm not a thrill-seeker, but I'll try almost anything once. I've ridden a bull, a camel, and even a shark, but I've never been on a horse.")
 alert("I have a strong hatred for onions, and a bad addiction to all things pumpkin-spiced.")
+
 let counter = 0;
 let answer = "wrong";
+
 function rainyQ(){
 let rain = prompt("Do I like the rain? Y/N").toLowerCase();
 if (rain === "yes" || rain === "y"){
@@ -22,7 +24,7 @@ if (rain === "yes" || rain === "y"){
 }
 console.log(`You got this question ${answer}!`);
 }
-rainyQ();
+//rainyQ();
 
 function saipanQ(){
 let saipan = prompt("Did I grow up in the East Coast Y/N").toLowerCase();
@@ -36,7 +38,7 @@ if (saipan === "yes" || saipan === "y"){
 }
 console.log(`You got this question ${answer}!`);
 }
-saipanQ();
+//saipanQ();
 
 function seattleQ(){
 let seattle = prompt("Was I born in Seattle? Y/N").toLowerCase();
@@ -50,12 +52,13 @@ if (seattle === "yes" || seattle === "y"){
 }
 console.log(`You got this question ${answer}!`);
 }
-seattleQ();
+//seattleQ();
 
 function poisonQ(){
 let poison = prompt("Onions have no place in this world! Y or Yes").toLowerCase();
 if (poison === "yes" || poison=== "y"){
     alert("Okay. That was a freebie.");
+    counter++;
 } else if (poison === "no" || poison === "n"){
     alert("You got that wrong. We're not compatable");
 } else{
@@ -63,7 +66,7 @@ if (poison === "yes" || poison=== "y"){
 }
 console.log(`You got this question ${answer}!`);
 }
-poisonQ();
+//poisonQ();
 
 function pumpkinQ(){
 let pumpkin = prompt("I hate all things pumpkin-spice... Y/N?").toLowerCase();
@@ -77,28 +80,87 @@ if (pumpkin === "yes" || pumpkin=== "y"){
 }
 console.log(`You got this question ${answer}!`);
 }
-pumpkinQ();
+//pumpkinQ();
 
 function animalsQ(){
 let chances =  4
-let animals = "4"
+let animals = 4
 
 for (let i = chances; i > 0; i--){
-    let guess1 = prompt("How many animals have I mentioned?");
-    if (guess1 === animals){
+    let guess1 = parseInt(prompt("How many animals have I mentioned?"));
+    
+     if (guess1 === animals){
         alert("Hey! You got it right!");
         counter++;
-        break;
+        i=0;
     }   else if (guess1 > animals){
-            alert(`Your guess was too high. You have ${i-1} chances left`);
+//            alert(`Your guess was too high. You have ${i-1} chances left.`);
+            if (i === 1){
+                alert("Sorry. You're out of chances. The answer was 4.");
+            }   else{
+                alert(`Your guess was too high. You have ${i-1} chances left.`);
+            }
     }   else if (guess1 < animals){
-            alert(`Your guess was too low. You have ${i-1} chances left`);
+//            alert(`Your guess was too low. You have ${i-1} chances left.`);
+            if (i === 1){
+                alert("Sorry. You're out of chances. The answer was 4.");
+            }   else{
+                alert(`Your guess was too low. You have ${i-1} chances left.`);
+            }
     }   else {
-            alert(`Sorry. I couldn't take that answer, but you still used a guess. You have ${i-1} chances left`);
+ //           alert(`Sorry. I couldn't take that answer, but you still used a guess. You have ${i-1} chances left.`);
+            if (i === 1){
+                alert("Sorry. You're out of chances. The answer was 4.");
+            }   else {
+                alert(`Sorry. I couldn't take that answer, but you still used a guess. You have ${i-1} chances left.`);
+            }
     }
 
 }
 }
-animalsQ();
+//animalsQ();
 
-alert(`You got ${counter}/7 questions right.`);
+function animalsQ2(){
+
+    let animals2 = ['bull','camel','horse','shark'];
+    let chances2 = 7
+    while (chances2){
+        let guess2 = prompt("Can you name one of the animals I mentioned earlier?").toLowerCase();
+        chances2--;
+        if (chances2 === 0){
+            alert("Sorry. You're all out of guesses. The answers were camel, bull, horse and shark.");
+        }   else if (guess2 === animals2[0] || guess2 === animals2[1] || guess2 === animals2[2] || guess2 === animals2[3]){
+            alert("Hey, you got one right.");
+            counter++;
+          chances2 = 0;
+        }   else {
+            alert(`Sorry. Your answer was wrong. Try again. you have ${chances2} guesses left.`)
+        }
+    
+    }
+}
+//animalsQ2();
+
+function quizMe(){
+    let text = "Would you like to take a quiz about me? Y/N";
+    if (confirm(text) == true) {
+        alert("Great. I'll ask you 7 questions. Let's start with question 1.");
+        rainyQ();
+        saipanQ();
+        seattleQ();
+        poisonQ();
+        pumpkinQ();
+        animalsQ();
+        animalsQ2();
+        alert(`You got ${counter}/7 questions right.`);
+        alert("Thank you for playing along. Welcome to my page.")
+      } 
+      else {
+        text = "That's alright. Welcome to my page.";
+        alert(text);
+      }
+}
+
+quizMe();
+
+//alert(`You got ${counter}/7 questions right.`);
